3ef626401d4280a861751b11f58ce87f7210314b
ammar <ammar.ali.haidar.1990@gmail.com>
2024-06-24 19:51:16 +0400

    main page

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -39,18 +39,18 @@ const MainSection = () => {
           </p>
         </h1>
         {/* paragraph */}
-        <p className="max-w-[800px] text-gray-600 mt-14 text-center text-base  md:text-2xl  font-semibold">
+        <p className="max-w-[800px] text-gray-600 mt-14 text-center text-base  sm:text-xl  font-semibold">
           We build top-tier websites, mobile apps, and SaaS applications using
           the most advanced tech stacks available.
         </p>
         {/* bullet points */}
-        <div className="flex items-center gap-8 mt-14 justify-start sm:justify-center flex-wrap w-fit self-center  ">
+        <div className="grid grid-cols-1 xs:grid-cols-2 mt-14 gap-y-1 gap-x-4 sm:gap-x-12 ">
           {bulletPoints.map((point) => (
             <h2
               key={point}
               className="flex items-center gap-2   text-gray-600 font-semibold text-xs sm:text-base "
             >
-              <CheckCircle size={16} />
+              <CheckCircle className="shrink-0" size={16} />
               {point}
             </h2>
           ))}
